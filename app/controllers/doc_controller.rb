6f16cb75cc71d22dd7554dce1ec5b073c1f67a07
Sirkka Antiala <sirkka.antiala@gmail.com>
2018-02-21 14:23:46 +0100

    Doc controller added

---
+++ app/controllers/doc_controller.rb
@@ -0,0 +1,22 @@
+class DocController < ApplicationController
+  def show
+  	# ...
+  end
+  def edit
+  	# ...
+  end
+  def update
+  	# ...
+  end
+  def destroy
+  	# ...
+  end
+
+  private
+
+  def find_doc
+  end
+
+  def doc_params
+  end
+end
