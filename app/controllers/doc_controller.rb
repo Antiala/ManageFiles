8ea6188c87c4d3487a1ed4c8c86d8938ce61c70c
Sirkka Antiala <sirkka.antiala@gmail.com>
2018-02-22 12:17:50 +0100

    final_application

--- app/controllers/doc_controller.rb
+++ app/controllers/doc_controller.rb
@@ -1,22 +1,49 @@
 class DocController < ApplicationController
-  def show
-  	# ...
-  end
-  def edit
-  	# ...
-  end
-  def update
-  	# ...
-  end
-  def destroy
-  	# ...
+  
+  def new
+    @doc = Doc.new
   end
 
-  private
-
-  def find_doc
+  def create
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
   end
+
+ private
 
   def doc_params
+    params.require(:doc).permit(:title, :content)
   end
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+ 
+  def show
+  end
+  #...
+
+  private
+ 
+ def find_doc
+	@doc = Doc.find(params[:id])
+ end
+ def index
+    @docs = Doc.all(“created_at_DESC”)
+ end
+
+ def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @doc.destroy
+    redirect_to docs_path
+  end
+
 end
